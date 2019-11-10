f69fac4936091568b6f11a10d8352b6cc1534a53
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-11-10 10:58:28 +0200

    first commit, created scene

---
+++ BottleFlip/GameViewController.swift
@@ -0,0 +1,49 @@
+//
+//  GameViewController.swift
+//  BottleFlip
+//
+//  Created by Kaiserdem on 10.11.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = MenuScene(fileNamed: "MenuScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .resizeFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true // дополнительная оптимизация рендеринга
+            
+            //view.showsFPS = true // частота обновления сцены
+            //view.showsNodeCount = true // число нодов
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
---
+++ BottleFlip/MenuScene.swift
@@ -0,0 +1,18 @@
+//
+//  MenuScene.swift
+//  BottleFlip
+//
+//  Created by Kaiserdem on 10.11.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene {
+
+  // когада сцена отобразилась
+  override func didMove(to view: SKView) {
+    
+    self.backgroundColor = #colorLiteral(red: 0.6588235294, green: 0.6980392157, blue: 0.7254901961, alpha: 1)
+  }
+}
